3de125392aa6c11ecb040d463eeadb20904a5f44
binjahid <muhammadjahidbinmutalib@gmail.com>
2022-05-16 21:42:27 +0600

    banner animation added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import Header from "../Header/Header";
 import bannerImg from "../../assets/images/logoAndBanner.png";
 
@@ -8,7 +10,22 @@ const Banner = () => {
       <div className="grid grid-cols-bannerGrid customContainer pt-20">
         <div className="flex justify-center items-center">
           <div>
-            <div className="text-7xl text-white font-bold">
+            <motion.div
+              initial={{
+                y: "-100vh",
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{
+                delay: 1,
+                type: "spring",
+                stiffness: 100,
+              }}
+              className="text-7xl text-white font-bold"
+            >
               <h2>
                 Play the{" "}
                 <span className="bg-secondary pb-[15px] px-4 rounded-xl">
@@ -16,21 +33,52 @@ const Banner = () => {
                 </span>
               </h2>
               <h2 className="pt-8">game of 2023 virtually</h2>
-            </div>
-            <p className="py-12 text-2xl text-gray-300">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea
-            </p>
-            <button className="text-white px-20 py-4 bg-tertiary text-2xl rounded-lg border-[.5rem] border-secondary">
-              Play Now
-            </button>
+            </motion.div>
+            <motion.div
+              initial={{
+                y: 300,
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{
+                delay: 0.5,
+                duration: 0.5,
+                type: "tween",
+              }}
+            >
+              <p className="py-12 text-2xl text-gray-300">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+              </p>
+              <button className="text-white px-20 py-4 bg-tertiary text-2xl rounded-lg border-[.5rem] border-secondary">
+                Play Now
+              </button>
+            </motion.div>
           </div>
         </div>
-        <div className="flex justify-center">
+        <motion.div
+          initial={{
+            y: 300,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{
+            delay: 0.8,
+            duration: 0.5,
+            type: "tween",
+          }}
+          className="flex justify-center"
+        >
           <img src={bannerImg} alt="banner" className="w-80 h-auto" />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,133 @@
 import logo from "../../assets/images/logoAndBanner.png";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+  BellIcon,
+  MenuIcon,
+  XIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/outline";
+import { PlusSmIcon } from "@heroicons/react/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Header = () => {
   return (
-    <header className="customContainer">
-      <div className="w-32">
-        <img src={logo} alt="logo" />
-      </div>
+    <header className="customContainer py-10">
+      <Disclosure as="nav" className="bg-transparent">
+        {({ open }) => (
+          <>
+            <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex justify-between h-16">
+                <div className="flex">
+                  <div className="-ml-2 mr-2 flex items-center md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <MenuIcon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                  <div className="flex-shrink-0 flex items-center">
+                    <img
+                      className="block lg:hidden w-20 h-auto"
+                      src={logo}
+                      alt="Workflow"
+                    />
+                    <img
+                      className="hidden lg:block w-32 h-auto"
+                      src={logo}
+                      alt="Workflow"
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="hidden md:ml-6 md:flex md:space-x-8">
+                    {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                    <a
+                      href="#"
+                      className=" text-gray-300 inline-flex items-center px-6 pt-1 text-2xl font-semibold hover:text-white duration-500"
+                    >
+                      Home
+                    </a>
+                    <a
+                      href="#"
+                      className=" text-gray-300 inline-flex items-center px-6 pt-1 text-2xl font-semibold hover:text-white duration-500"
+                    >
+                      App
+                    </a>
+                    <a
+                      href="#"
+                      className=" text-gray-300 inline-flex items-center px-6 pt-1 text-2xl font-semibold hover:text-white duration-500"
+                    >
+                      NFT
+                    </a>
+                    <a
+                      href="#"
+                      className=" text-gray-300 inline-flex items-center px-6 pt-1 text-2xl font-semibold hover:text-white duration-500"
+                    >
+                      Marchandise
+                    </a>
+                  </div>
+
+                  <div className=" md:ml-4 md:flex-shrink-0 md:flex md:items-center mx-6">
+                    <button type="button" className="text-white">
+                      <ShoppingBagIcon className="h-8 w-8" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="hidden md:block flex-shrink-0">
+                    <button className="text-white px-12 py-2 bg-tertiary text-2xl rounded-lg border-[.5rem] border-secondary">
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="md:hidden">
+              <div className="pt-2 pb-3 space-y-1">
+                {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                >
+                  Dashboard
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                >
+                  Team
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                >
+                  Projects
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                >
+                  Calendar
+                </Disclosure.Button>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </header>
   );
 };
